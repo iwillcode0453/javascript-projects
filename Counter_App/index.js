0c83d3f56edf718da58b458947f70887834a2f0d
Erickson O. Dumaran <ericagustin23@gmail.com>
2025-11-14 00:24:54 +0800

    put comments on every line of function

--- Counter_App/index.js
+++ Counter_App/index.js
@@ -8,19 +8,20 @@ const saveBtn = document.getElementById("save-btn");
 const resetBtn = document.getElementById("reset-btn");
 
 incrementBtn.addEventListener("click", function() {
-    count += 1;
-    countEl.textContent = count;
+    count += 1; //when the icrement button is clicked, add 1 to count
+    countEl.textContent = count; //update the count-el in the HTML to reflect the niew count value on the page
 });
 
 saveBtn.addEventListener("click", function() {
-    saveEl.textContent += count + " - ";
-    count = 0;
-    countEl.textContent = count;
+    saveEl.textContent += count + " - "; //append the current count value followed by the hypen to the save-el element
+    count = 0; // reset the count to 0
+    countEl.textContent = count; //update the countEl element to show the reset count value 0
 })
 
 resetBtn.addEventListener("click", function() {
-    count = 0;
-    saveEl.textContent = "Previous entries: "
+    count = 0; //when clicked, count will be reeset to 0
+    saveEl.textContent = "Previous entries: "; // clear the saveEl content on the page
+    countEl.textContent = count; //update the countEl element to show the reset count value 0
 })
 
 

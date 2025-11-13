7dbbd479bee3ea75d250af89819fadd40d9dc34b
Erickson O. Dumaran <ericagustin23@gmail.com>
2025-11-13 23:52:56 +0800

    modified the js file esp. the function

--- Counter_App/index.js
+++ Counter_App/index.js
@@ -3,12 +3,20 @@
 let count = 0;
 const countEl = document.getElementById("count-el");
 const incrementBtn = document.getElementById("increment-btn");
+const saveEl = document.getElementById("save-el");
 const saveBtn = document.getElementById("save-btn");
 
 incrementBtn.addEventListener("click", function() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 });
+
+saveBtn.addEventListener("click", function() {
+    saveEl.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = count;
+})
+
 
 // let counting = 0;
 // let countElem = document.getElementById("count-el");
@@ -17,3 +25,4 @@ incrementBtn.addEventListener("click", function() {
 //     counting += 1;
 //     countElem.innerText = counting;
 // }
+

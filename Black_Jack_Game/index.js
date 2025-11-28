16df756ac6f000f3bce7e58a7eae3bcaf023758f
Erickson O. Dumaran <ericagustin23@gmail.com>
2025-11-29 02:02:19 +0800

    update javascript and style

--- Black_Jack_Game/index.js
+++ Black_Jack_Game/index.js
@@ -1,8 +1,25 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-let firstCard = Math.floor(Math.random() * 10) + 2;
-let secondCard = Math.floor(Math.random() * 10) + 2;
-// 2. Create a variable, sum, and set it to the sum of the two cards
-let sum = firstCArd + secondCard;
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-console.log(`First Card: ${firstCArd}, Second Card: ${secondCard}`);
+// Select the start-btn element
+let startBtn = document.getElementById("start-btn"); 
+
+// function to start the game
+function startGame() {
+    if (sum < 21) {
+			message = "Do you want to draw a new card?";
+	} else if (sum === 21) {
+		    message = "Wohoo! You've got Blackjack!";
+			hasBlackJack = true;
+	} else {
+			message = "You're out of the game!";
+			isAlive = false;
+	}
+
+	console.log(message);
+}
+

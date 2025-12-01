d561d9b7605c72aed04219a87f2d5a8cff5d1ca0
Erickson O. Dumaran <ericagustin23@gmail.com>
2025-12-01 23:51:20 +0800

    added some buttons and fucntion

--- Black_Jack_Game/index.js
+++ Black_Jack_Game/index.js
@@ -7,14 +7,22 @@ let message = "";
 
 
 let startBtn = document.getElementById("start-btn"); 
+let newCardBtn = document.getElementById("new-card-btn");
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
 
 // function to start the game
 function startGame() {
+	// Display thea cards value with cardsel variable
+    cardsEl.textContent = "Cards: " + firstCard + "," + secondCard;
+	// Display the sum of the cards with sumEl variable
+	sumEl.textContent = "Sum: " + sum;
     if (sum < 21) {
 			message = "Do you want to draw a new card?";
 	} else if (sum === 21) {
-		    message = "Wohoo! You've got Blackjack!";
+		    message = "You've got Blackjack!";
 			hasBlackJack = true;
 	} else {
 			message = "You're out of the game!";
@@ -25,3 +33,7 @@ function startGame() {
 	
 }
 
+// Function to draw a new card with newCardBtn varialble
+function newCard() {
+	console.log("Draw another card!")
+}

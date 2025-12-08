5ed81ef68dbe672274ee264bb629517ea8207915
Erickson O. Dumaran <ericagustin23@gmail.com>
2025-12-08 23:39:38 +0800

    this is a practice js file

--- Challenge script/index.js
+++ Challenge script/index.js
@@ -1,6 +1,46 @@
-let purchaseButton = document.getElementById('purchase-button');
-let errorMessage = document.getElementById('error');
+// let purchaseButton = document.getElementById('purchase-button');
+// let errorMessage = document.getElementById('error');
 
-purchaseButton.addEventListener('click', function() {
-    errorMessage.textContent = 'Something went wrong, please try again later!'
-})
+// purchaseButton.addEventListener('click', function() {
+//     errorMessage.textContent = 'Something went wrong, please try again later!'
+// })
+
+// let cards = [7, 3, 5]
+
+// for (let i = 0; i < cards.length; i += 1) {
+//     console.log(cards[i])
+// }
+
+let sentence = ["Hello", "my", "name", "is", "Per"];
+let greetingEl = document.getElementById("greeting-el");
+
+// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+
+for (let i = 0; i < sentence.length; i++) {
+	greetingEl.textContent += sentence[i] + " ";
+	console.log(greetingEl.textContent);
+}
+
+const personObject = {
+	name: "Erickson",
+	age: 47,
+	country: "Philippines"
+}
+
+function logData() {
+	console.log(`${personObject.name} is ${personObject.age} years old and lives in the ${personObject.country}.`)
+}
+
+let age = 15;
+
+if (age < 6) {
+	console.log("Free");
+} else if(age < 18) {
+	console.log("Child Discount")
+} else if(age < 27) {
+	console.log("Student Discount")
+} else if (age < 67) {
+	console.log("Full Price")
+} else {
+	console.log("Senior Citizen Discount")
+}

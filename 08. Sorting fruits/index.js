9b95eebc569a285aa8f0f509c569e38ab4d07c75
Erickson O. Dumaran <ericagustin23@gmail.com>
2026-01-28 01:43:25 +0800

    update the function of sortFruit and also the css file

--- 08. Sorting fruits/index.js
+++ 08. Sorting fruits/index.js
@@ -1,15 +1,26 @@
 let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
 let appleShelf = document.getElementById("apple-shelf")
 let orangeShelf = document.getElementById("orange-shelf")
+let sortFruitBtn = document.getElementById("sortFruit")
 
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
 
-for (let i = 0; i < fruit.length; i++) {
-  if (fruit[i] === "🍎") {
-    appleShelf.textContent += fruit[i]
-  } else if (fruit[i] === "🍊") {
-    orangeShelf.textContent += fruit[i]
-  }
-}
+  // declare a sorted variable to false to run the sorting
+  let sorted = false;
+
+  sortFruitBtn.addEventListener("click", function() {
+    for (let i = 0; i < fruit.length; i++) {
+      if (!sorted) {
+         if (fruit[i] === "🍎") {
+						appleShelf.textContent += fruit[i];
+					} else if (fruit[i] === "🍊") {
+						orangeShelf.textContent += fruit[i];
+					} 
+      }
+      
+    }
+    // set sorted variable to true to prevent re-sorting; it will be labeled to false again and not run the sorting code again
+    sorted = true; 
+  });

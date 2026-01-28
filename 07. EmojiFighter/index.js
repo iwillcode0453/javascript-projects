181e80572b1f407c35412d60a614e91d7390bad0
Erickson O. Dumaran <ericagustin23@gmail.com>
2026-01-29 01:39:42 +0800

    created a animal fighter random selector

--- 07. EmojiFighter/index.js
+++ 07. EmojiFighter/index.js
@@ -7,4 +7,10 @@ fightButton.addEventListener("click", function() {
     // Challenge:
     // When the user clicks on the "Pick Fighters" button, pick two random 
     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let fighter1 = Math.floor(Math.random() * fighters.length);
+    let fighter2 = Math.floor(Math.random() * fighters.length);
+    // randomly select two animals from the array
+    stageEl.textContent = fighters[fighter1] + " vs " + fighters[fighter2];
+    console.log(fighters[fighter1] + " vs " + fighters[fighter2]);
 })
+// Hints:
